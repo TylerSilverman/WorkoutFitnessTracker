5de2c309e8a2757a30619fe26d118e4b55237208
Tyler Silverman <silverman.tyler@gmail.com>
2021-01-22 17:05:21 -0500

    added routes to the api.js file

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const Workout = require("../models/Workout.js");
 
 router.get('/workouts', (req, res) => {
-    Workout.find({})
+    Workout.aggregate({})
+        .sort({day: 1})
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
@@ -11,14 +12,37 @@ router.get('/workouts', (req, res) => {
         });
 })
 
-// router.put
+// router.get('/workouts', (req, res)=>{
+//     Workout.aggregate([{
+//         tot
+//     }])
+// })
+//this workouts/id is from the api.js on line 13
+// router.put('/workouts/:id', (req, res)=>{
+//     console.log("hitting the add exercise btn")
+//     res.json({update: true})
+// })
 
-// req.body.type === "resistance"
-//req.body
-//req.body
-//req.body
 
-// req.body.type === "cardio"
+//backend code
+router.put('/workouts/:id', (req, res) => {
+    const id = req.params.id
+    const body = req.body
+
+    Workout.findByIdAndUpdate(id,
+        {
+            $push: {
+                exercises: body
+            }
+        }
+    )
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.status(400).json(err.message);
+        });
+})
 
 router.post("/workouts", ({ body }, res) => {
     console.log("post workout btn")
